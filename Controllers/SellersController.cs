1d263543fee3d9a72d9ebe8cc319f173ae2387a7
Gabriel Brassaroto <gabriel.brassaroto@tcs.com>
2020-06-15 16:03:30 -0300

    SellerService and basic FindAll

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerService; // importa o servido 
+
+        public SellersController(SellerService sellerService)// o contrutor para acessar o servico
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerService.FindAll();/// faz a pesquisa
+            return View(list);/// retorna a lista para view 
         }
     }
 }
---
+++ Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;//criar o objeto de contexto
+
+        public SellerService(SalesWebMvcContext context)///contrutur para pode fazer injecao 
+        {
+            _context = context;/// passa para o contexto
+        }
+
+        public IList<Seller> FindAll()//pesquisa tudo 
+        {
+            return _context.Seller.ToList(); //// pega todos os registro do banco 
+        }
+    }
+}
